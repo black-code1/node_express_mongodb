13032a44f70ca1fb29bc711c60c76361ec76983f
black-code1 <legrandmahamat1@gmail.com>
2021-07-30 01:07:57 +0100

    Movie App PART 2

--- MovieApp/app.js
+++ MovieApp/app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
+app.get('/results', (req, res) => res.render('movies'))
 app.get('/search', (req, res) => res.render('search'))
 
 app.listen(3000, () => console.log('Server started at port 3000.'))
